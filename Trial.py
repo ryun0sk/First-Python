93d8429cdb6ded84afabf9fce9ce605e843ce11e
ryu <>
2019-01-12 09:28:07 +0900

    First commit

---
+++ Trial.py
@@ -0,0 +1,14 @@
+#%%
+
+price = [100, 250, 300, 500, 700]
+number = [1, 2, 3, 4, 5]
+
+plt.plot(price, number)
+
+plt.title("price / number")
+
+plt.xlabel("price")
+
+plt.ylabel("number")
+
+plt.show()
